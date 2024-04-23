3a3d9daecb9e4de2aba517a03882b599996b3db1
taketoiida <taketo.iida.e379@dirbato.co.jp>
2024-04-23 12:19:41 +0900

    存在しないidに対する処理を追加

--- msbd/src/controllers/EditServlet.java
+++ msbd/src/controllers/EditServlet.java
@@ -31,8 +31,10 @@ public class EditServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // メッセージIDをセッションスコープに登録
+        if(m != null) {
         request.getSession().setAttribute("message_id", m.getId());
-
+        }
+        
         var rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
     }
